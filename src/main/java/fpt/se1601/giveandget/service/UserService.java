3bec3d7e8885a5bb34d22597da583aa92bd809a6
raven <trannhathoang8678@gmail.com>
2021-06-05 20:54:00 +0700

    add UserService

---
+++ src/main/java/fpt/se1601/giveandget/service/UserService.java
@@ -0,0 +1,8 @@
+package fpt.se1601.giveandget.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+}
