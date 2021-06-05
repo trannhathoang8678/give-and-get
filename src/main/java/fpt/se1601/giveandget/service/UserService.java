a4108dc5eefc17d3aa2f9997e53bdf38a5df0963
raven <trannhathoang8678@gmail.com>
2021-06-05 21:16:18 +0700

    set up swagger

--- src/main/java/fpt/se1601/giveandget/service/UserService.java
+++ src/main/java/fpt/se1601/giveandget/service/UserService.java
@@ -4,5 +4,5 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserService  {
 }
